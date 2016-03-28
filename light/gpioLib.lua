76d1dde8eec9c3e213311adf23eabfc80d2a0589
pikous <tomas.pikous@gmail.com>
2016-03-28 22:48:38 +0200

    GPIO works

--- light/gpioLib.lua
+++ light/gpioLib.lua
@@ -2,12 +2,32 @@ local this = _G
 module("gpioLib")
 
 local print = this.print
+local gpio = this.gpio
+local pwm = this.pwm
+
+local digitalPin = 2
+local pwmPin = 7
+local pwmClock = 120
+
+function init()
+    gpio.mode(digitalPin, gpio.OUTPUT)
+    pwm.setup(pwmPin,pwmClock,0)
+end
 
 function setPower(state,power)
     if(power>=0)then
         if(power<256)then
-            state.power=power
-            print("setting power to "..power) 
+            state.power=power*4
+            print("setting power to "..state.power) 
+
+
+            value=0
+            if(state.power>0)then
+                value=value+1
+            end
+            gpio.write(digitalPin,value)
+            pwm.setduty(pwmPin,state.power)
+            
             return true
         end   
     end
--- light/init.lua
+++ light/init.lua
@@ -8,6 +8,8 @@ client_id = string.format("%X%X",node.chipid(),node.flashid())
 client_name = string.format("ESP_%s",client_id)
 print(client_name)
 
+gpioLib.init()
+
 mqttLib.init(client_id,gpioLib)
 wifiLib.init()
 wifiLib.setupListeners(mqttLib)
